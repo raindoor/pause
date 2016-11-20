4cbe466b945b84fdb29fab8157ad76206db2e579
Raindoor <raindoor38@gmail.com>
2016-11-20 13:05:37 +0900

    intro2 완료

--- web/js/intro.js
+++ web/js/intro.js
@@ -1,12 +1,25 @@
 // Agency Theme JavaScript
 var cnt = 1;
+var redirectCnt = 0;
+var changeTimer;
 (function($) {
-  setInterval(changeIntroWord , 1000);
+  changeTimer = setInterval(changeIntroWord , 1000);
 
 })(jQuery); // End of use strict
 
 function changeIntroWord(){
+  if(redirectCnt == 5){
+    clearInterval(changeTimer);
+    showIntro2();
+    return;
+  }
   $('.wordContainer h1').hide();
   $('.wordContainer .word'+(cnt+1)).show();
   cnt = (cnt+1)%3;
+  redirectCnt++;
+}
+
+function showIntro2(){
+  $('.introContainer').hide();
+  $('.intro2Container').show();
 }
