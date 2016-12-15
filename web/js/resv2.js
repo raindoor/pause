bcb253c3054a2bb4c9384417823e26666056bfeb
Raindoor <raindoor38@gmail.com>
2016-12-15 16:46:27 +0900

    자잘한 수정

--- web/js/resv2.js
+++ web/js/resv2.js
@@ -14,7 +14,13 @@ var movieInfo = [
       "../images/resv/1/1-large2.png",
       "../images/resv/1/1-7.png",
       "../images/resv/1/1-8.png",
-    ]
+    ],
+    movieTitle : "인셉션",
+    movieTitleEng : "Inception",
+    movieYear : "2010",
+    movieTime : "1시간 47분",
+    movieDirector : "크리스토퍼 놀란",
+    movieDiscription : "타인의 꿈속에 침투해 생각을 훔칠 수 있는 가까운 미래, 이 분야 최고 실력자 ‘코브’(레오나르도 디카프리오)는 아내를 살해했다는 누명을 쓴 채 도망자가 된다. 어느 날, 거대기업 후계자의 머리 속에 새로운 생각을 심어 기업의 합병을 막아달라는 의뢰를 받는다. 의뢰인이 내건 조건은 거액의 돈과 코브가 집으로 돌아갈 수 있도록 돕겠다는 것. \n\n코브는 대단한 창의력으로 꿈의 공간을 설계하는데 놀라운 재능을 가진 건축학도를 비롯해 최고의 실력을 갖춘 5명의 드림팀을 조직해 작전에 나선다. 작전명 ‘인셉션’! 각각의 임무를 맡은 그들에게 주어진 미션, 표적의 머릿속에 생각을 입력하라!"
   },
   {text :
     [ "어톤먼트","케빈에 대하여","빅피쉬","레미제라블","서브마린",
@@ -31,11 +37,17 @@ var movieInfo = [
       "../images/resv/2/2-large2.png",
       "../images/resv/2/2-7.png",
       "../images/resv/2/2-8.png",
-    ]
+    ],
+    movieTitle : "어톤먼트",
+    movieTitleEng : "Atonement",
+    movieYear : "2007",
+    movieTime : "2시간 10분",
+    movieDirector : "조 라이트",
+    movieDiscription : "1935년 영국, 부유한 집안의 아름다운 딸 세실리아(키이라 나이틀리)는 시골 저택에서 여름을 보내던 중 가정부의 아들이자 세실리아 집안의 도움으로 캠브리지 의대를 졸업한 로비(제임스 맥어보이)와 마주친다. 어릴 때부터 서로에게 애틋한 마음이 있었지만 서로의 신분 차이 때문에 애써 자신의 마음을 부정하며 고백하지 못하던 이들은 그날 밤 서로의 사랑을 확인한다. 하지만 이들을 지켜본 세실리아의 동생 브라이오니의 오해로 로비는 억울한 누명을 쓰고 전쟁터로 끌려가게 된다. 이후 세실리아는 로비가 전쟁터에서 돌아오기만을 기다리며 간호사로 일하게 되고, 로비 또한 세실리아를 다시 만난다는 단 하나의 일념으로 전쟁터에서 살아남기 위해 애쓰는데…"
   },
   {text :
-    [ "어톤먼트","케빈에 대하여","빅피쉬","레미제라블","서브마린",
-    "무드 인디고","멜랑꼴리아","대니쉬걸","아멜리에","문라이즈킹덤"],
+    [ "더 랍스터","루비 스팍스","싱스트리트","그랜드\n부다페스트호텔","원데이",
+    "빅쇼트","굿월헌팅","신비한 동물사전","주토피아","동주"],
     url :
     [
       "../images/resv/3/3-large1.png",
@@ -48,54 +60,77 @@ var movieInfo = [
       "../images/resv/3/3-large2.png",
       "../images/resv/3/3-7.png",
       "../images/resv/3/3-8.png",
-    ]
+    ],
+    movieTitle : "더랍스터",
+    movieTitleEng : "The Lobster",
+    movieYear : "2015",
+    movieTime : "1시간 59분",
+    movieDirector : "요르고스 란티모스",
+    movieDiscription : "가까운 미래, 모든 사람들은 서로에게 완벽한 짝을 찾아야만 한다. 홀로 남겨진 이들은 45일간 커플 메이킹 호텔에 머무르며, 완벽한 커플이 되기 위한 교육을 받는다. 그럼에도 불구하고 짝을 얻지 못한 사람은 동물로 변해 영원히 숲 속에 버려지게 된다.\n\n근시란 이유로 아내에게 버림받고 호텔로 오게 된 데이비드(콜린 파렐)는 새로운 짝을 찾기 위해 노력하지만 결국 참지 못하고 숲으로 도망친다. 숲에는 커플을 거부하고 혼자만의 삶을 선택한 솔로들이 모여 살고 있다. 솔로 생활을 유지하기 위한 그들의 절대규칙은 바로 절대 사랑에 빠지지 말 것!\n아이러니하게도 데이비드는 사랑이 허락되지 않는 그곳에서 자신과 같이 근시를 가진 완벽한 짝(레이첼 와이즈)을 만나고 마는데..!"
   }
 ]
 $(document).ready(function(){
   var params = getQueryParams(document.URL);
-  var dateInfo = params.mm+"월 "+params.dd+"일 "+params.word+", "+params.dayNight+" "+params.time+"시";
+  var par_mm = params.mm ? params.mm : "";
+  var par_dd = params.dd ? params.dd : "";
+  var par_word = params.word ? params.word : "";
+  var par_dayNight = params.dayNight ? params.dayNight : "";
+  var par_time = params.time ? params.time : "";
+  var dateInfo = par_mm+"월 "+par_dd+"일 "+par_word+", "+par_dayNight+" "+par_time+"시";
   $('.movieInfo').text(dateInfo);
   $('input[name=dateInfo]').val(dateInfo);
   $('.movieUnit').click(function(){
     if($(this).attr('movieUrl'))
       changeMovieVideo($(this).attr('movieUrl'));
   });
+  var dateType;
   switch (params.word) {
-    case "오늘":
-      $('.movieUnit').each(function(idx){
-        if(idx == 3 || idx == 5)
-          $(this).css('padding-top','50px');
-        else if(idx == 0)
-          $(this).attr('movieUrl','https://www.youtube.com/embed/V3-a58Wt2tk').click();
-        $(this).text(movieInfo[0].text[idx]);
-        $(this).css('background-image','url('+movieInfo[0].url[idx]+')');
-      });
-      break;
     case "내일":
       $('.movieUnit').each(function(idx){
+        dateType = 1;
         if(idx == 0)
           $(this).attr('movieUrl','https://www.youtube.com/embed/IkF3M_FE4MM').click();
-        $(this).text(movieInfo[1].text[idx]);
-        $(this).css('background-image','url('+movieInfo[1].url[idx]+')');
+        $(this).text(movieInfo[dateType].text[idx]);
+        $(this).css('background-image','url('+movieInfo[dateType].url[idx]+')');
       });
       break;
     case "모레":
       $('.movieUnit').each(function(idx){
+        dateType = 2;
+        if(idx == 3)
+          $(this).css('padding-top','50px');
         if(idx == 0)
           $(this).attr('movieUrl','https://www.youtube.com/embed/1Fg5iWmQjwk').click();
-        $(this).text(movieInfo[2].text[idx]);
-        $(this).css('background-image','url('+movieInfo[2].url[idx]+')');
+        $(this).text(movieInfo[dateType].text[idx]);
+        $(this).css('background-image','url('+movieInfo[dateType].url[idx]+')');
       });
       break;
     default:
-
+      $('.movieUnit').each(function(idx){
+        dateType = 0;
+        if(idx == 3 || idx == 5)
+          $(this).css('padding-top','50px');
+        else if(idx == 0)
+          $(this).attr('movieUrl','https://www.youtube.com/embed/vU29VfayDMw').click();
+        $(this).text(movieInfo[dateType].text[idx]);
+        $(this).css('background-image','url('+movieInfo[dateType].url[idx]+')');
+      });
+      break;
   }
+
+  $('.selectedMovieInfo .movieTitle').text(movieInfo[dateType].movieTitle);
+  $('.selectedMovieInfo .movieTitleEng').text(movieInfo[dateType].movieTitleEng);
+  $('.selectedMovieInfo .movieYear').text(movieInfo[dateType].movieYear);
+  $('.selectedMovieInfo .movieTime').text(movieInfo[dateType].movieTime);
+  $('.selectedMovieInfo .movieDirector').text(movieInfo[dateType].movieDirector);
+  $('.selectedMovieInfo .movieDiscription').text(movieInfo[dateType].movieDiscription);
+
   $("body").smoothWheel();
 });
 
 function getQueryParams(qs) {
 
-  qs = qs.split('?')[1].split('+').join(' ');
+  qs = qs && qs.indexOf('?') != -1 ? qs.split('?')[1].split('+').join(' ') : {};
 
   var params = {},
   tokens,
